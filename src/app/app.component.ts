2b0c3965e8fb02f5225da0949b08296b2595f9e0
Tarun <tarun.mail09@gmail.com>
2020-12-04 10:42:46 +1100

    Added spinner and load more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,22 +17,30 @@ export class AppComponent {
   posts = [];
   comments = [];
   selectedPost = null;
-
+  showLoader =  false;
   dataSource: any[] = [];
 
+  showLoadMoreButton = true;
   constructor(private getInfo: SchoolinTestService) {
     this.getUsers();
   }
 
   getUsers() {
+    this.showLoader = true;
     this.getInfo.getusers().subscribe((data) => {
       console.log(data)
-      this.users = data;
-      this.users.forEach(user => { user["firstName"] = user.name.replace(/ .*/, ''); });
+      setTimeout(a=>{
+        this.users = data;
+        this.users.forEach(user => { user["firstName"] = user.name.replace(/ .*/, ''); });
+        this.showLoader = false;
+      },2000,[]);
+
+      
     })
   }
 
   selectUser(user) {
+    this.showLoadMoreButton = true;
     this.posts = [];
     this.comments = [];
     this.selectedUser = user;
@@ -41,16 +49,32 @@ export class AppComponent {
 
   getPosts() {
     if (this.selectedUser) {
+      this.showLoader = true;
       this.getInfo.getPosts().subscribe((data) => {
-        this.posts = data.filter(p => p.userId === this.selectedUser.id);
+        setTimeout(a=>{
+          this.posts = data.filter(p => p.userId === this.selectedUser.id);
+          this.showLoader = false;
+        },2000,[]);
       });
     }
   }
 
   getComments(post) {
     this.comments = [];
+    this.showLoader = true;
     this.getInfo.getComments().subscribe((data) => {
-      this.comments = data.filter(c => c.postId === post.id);
+      setTimeout(a=>{
+        this.comments = data.filter(c => c.postId === post.id);
+        this.showLoader = false;
+      },2000,[]);
     });
+  }
+
+  loadMore() {
+    this.showLoader = true;
+    setTimeout(a=>{
+      this.showLoadMoreButton = !this.showLoadMoreButton;
+      this.showLoader = false;
+    },2000,[]);
   }
 }
